c1ed595bf2ef7f8ee56484d73302336522fd851b
magame2310 <161406463+magame2310@users.noreply.github.com>
2024-09-10 21:56:36 -0500

    Punto 2

---
+++ Punto 2/mavenproject1/src/main/java/Figuras/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Figuras;
+import Figuras.VentanaPrincipal;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; /* Define la ventana
+        principal */
+        miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana
+        principal */
+        miVentanaPrincipal.setVisible(true); /* Establece la ventana
+        como visible */
+        // Establece que la ventana no puede cambiar su tamaño
+        miVentanaPrincipal.setResizable(false);
+    }
+}
